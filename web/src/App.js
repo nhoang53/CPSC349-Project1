75ef91f9aae67e1085125d8d17394a18140c89d4
DENNIS KIEU <42823136+daikieu95@users.noreply.github.com>
2019-10-09 15:46:15 -0700

    Add files via upload

--- web/src/App.js
+++ web/src/App.js
@@ -8,6 +8,9 @@ import NotFoundPage from "./components/pages/NotFoundPage";
 import HomePage from "./components/pages/HomePage";
 import PricingPage from "./components/pages/PricingPage";
 import RegisterPage from "./components/pages/RegisterPage";
+import LoginPage from "./components/pages/LoginPage";
+import ContactPage from "./components/pages/ContactPage";
+import ForgotPwdPage from "./components/pages/ForgotPwdPage";
 
 import Footer from "./components/templates/Footer";
 
@@ -20,6 +23,9 @@ function App() {
         <Route path="/not-found" component={NotFoundPage} />
         <Route path="/pricing" component={PricingPage} />
         <Route path="/register" component={RegisterPage} />
+        <Route path="/login" component={LoginPage} />
+        <Route path="/contact" component={ContactPage} />
+        <Route path="/forgotpwd" component={ForgotPwdPage} />
         <Route exact path="/" component={HomePage} />
         <Redirect to="/not-found" />
       </Switch>
