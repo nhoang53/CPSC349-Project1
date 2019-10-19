400d262b7677ae322d58256d4c119784d2e38b45
Kedyn Macedonio <mkedyn@gmail.com>
2019-10-19 05:00:01 -0700

    redesigned forgot password, added validation to it

--- web/src/components/pages/ForgotPasswordPage.jsx
+++ web/src/components/pages/ForgotPasswordPage.jsx
@@ -1,53 +1,111 @@
 import React, { Component } from "react";
-import { Container, Card, Form, Row, Col } from "react-bootstrap";
+import {
+  Container,
+  Card,
+  Form,
+  Row,
+  Col,
+  Button,
+  Spinner
+} from "react-bootstrap";
 
 export default class ForgotPasswordPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      validated: false,
+      checking: false,
+      email: ""
+    };
+  }
+
+  isValid(form) {
+    const data = new FormData(form);
+    const email = data.get("email");
+
+    let valid = false;
+
+    form[0].setCustomValidity("Invalid");
+
+    if (!email) {
+      this.setState({ email: "Please enter a your email." });
+    } else {
+      if (!/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/.test(email)) {
+        this.setState({ email: "Please enter a valid email." });
+      } else {
+        form[0].setCustomValidity("");
+        valid = true;
+      }
+    }
+
+    this.setState({ validated: true });
+
+    return valid;
+  }
+
+  onSubmit = event => {
+    event.preventDefault();
+
+    const form = event.target;
+
+    if (form.checkValidity() === false) {
+      event.stopPropagation();
+    }
+
+    if (this.isValid(form)) {
+      this.setState({ checking: true });
+      console.log("Call api to check to get token");
+    }
+  };
+
   render() {
+    const { validated, checking, email } = this.state;
+
     return (
       <main role="main">
-        <Container className="my-5 text-center">
-          <h1 className="my-5">Forgot Password</h1>
+        <Container className="my-5">
+          <h1 className="my-5 text-center">Forgot Password</h1>
 
           <Card>
             <Card.Body>
-              <Form>
+              <p className="mb-4">
+                If you forgot your password, enter your email and we will send
+                you instruction on how to reset your password.
+              </p>
+              <Form noValidate validated={validated} onSubmit={this.onSubmit}>
                 <Row className="mb-4">
                   <Col>
                     <Form.Control
-                      type="text"
-                      name="username"
-                      placeholder="Username"
+                      type="email"
+                      name="email"
+                      placeholder="Email"
                       required
                     />
+                    <Form.Control.Feedback type="invalid">
+                      {email}
+                    </Form.Control.Feedback>
                   </Col>
                 </Row>
-                <Row className="mb-4">
-                  <Col>
-                    <Form.Control
-                      type="password"
-                      name="password"
-                      placeholder="New Password"
-                      required
-                    />
+                {checking && (
+                  <Row className="mb-4 text-center">
+                    <Col>
+                      <Spinner animation="grow" />
+                    </Col>
+                  </Row>
+                )}
+                <Row>
+                  <Col className="text-center">
+                    <Button
+                      className="mb-4"
+                      variant="primary"
+                      type="submit"
+                      block
+                    >
+                      Reset Password
+                    </Button>
                   </Col>
                 </Row>
-                <Row className="mb-4">
-                  <Col>
-                    <Form.Control
-                      type="password"
-                      name="repeat password"
-                      placeholder="Repeat Password"
-                      required
-                    />
-                  </Col>
-                </Row>
-                <button
-                  type="submit"
-                  class="btn btn-primary"
-                  name="rspwd-submit"
-                >
-                  Confirm
-                </button>
               </Form>
             </Card.Body>
           </Card>
