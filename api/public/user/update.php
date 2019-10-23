d8bfc655b4407698759bb4ebcca4064ecab37141
Kedyn <mkedyn@gmail.com>
2019-10-22 20:05:43 -0700

    minor fix

--- api/public/user/update.php
+++ api/public/user/update.php
@@ -29,73 +29,73 @@ if (!empty($_POST["fullName"]) &&
         $user = new User($db);
 
         if ($user->exists("email", $_POST["email"])) {
-                if (move_uploaded_file($imageTmpName, $uploadName)) {
+            if (move_uploaded_file($imageTmpName, $uploadName)) {
 
-                    $user->fullName = $_POST["fullName"];
-                    $user->email = $_POST["email"];
-                    //$user->password = $_POST["password"];
-                    $user->image = $uploadUrl;
-                    $user->link = $link;
-                    $user->location = $_POST["location"];
-                    $user->ocupation = $_POST["ocupation"];
-                    $user->summary = $_POST["summary"];
+                $user->fullName = $_POST["fullName"];
+                $user->email = $_POST["email"];
+                //$user->password = $_POST["password"];
+                $user->image = $uploadUrl;
+                $user->link = $link;
+                $user->location = $_POST["location"];
+                $user->ocupation = $_POST["ocupation"];
+                $user->summary = $_POST["summary"];
 
-                    if (!empty($_POST["github"])) {
-                      $user->update = $_POST["github"];
-                    }
-                    if (!empty($_POST["linkedIn"])) {
-                      $user->update = $_POST["linkedIn"];
-                    }
-                    if (!empty($_POST["facebook"])) {
-                      $user->update = $_POST["facebook"];
-                    }
-                    if (!empty($_POST["twitter"])) {
-                      $user->update = $_POST["twitter"];
-                    }
-                    if (!empty($_POST["instagram"])) {
-                      $user->update = $_POST["intagram"];
-                    }
-                    if (!empty($_POST["youtube"])) {
-                      $user->update = $_POST["youtube"];
-                    }
-
-                    if ($user->update()) {
-                      $token = array(
-                        "iss" => $iss,
-                        "aud" => $aud,
-                        "iat" => $iat,
-                        "nbf" => $nbf,
-                        "data" => array(
-                            "fullName" => $user->fullName,
-                            "email" => $user->email,
-                            "image" => $user->image,
-                            "link" => $user->link,
-                            "summary" => $user->summary,
-                            "location" => $user->location,
-                            "ocupation" => $user->ocupation,
-                            "pro" => $user->pro,
-                            "github" => $user->github,
-                            "linkedIn" => $user->linkedIn,
-                            "facebook" => $user->facebook,
-                            "twitter" => $user->twitter,
-                            "instagram" => $user->instagram,
-                            "youtube" => $user->youtube,
-                            "projects" => $user->projects,
-                          ),
-                      );
-        
-                        $jwt = JWT::encode($token, $key);
-
-                        http_response_code(200);
-                        echo json_encode(array("message" => "Success.", "jwt" => $jwt));
-                    } else {
-                        http_response_code(202);
-                        echo json_encode(array("message" => "Unable to create user."));
-                    }
-                } else {
-                    http_response_code(500);
-                    echo json_encode(array("message" => "Unable to upload image."));
+                if (!empty($_POST["github"])) {
+                  $user->update = $_POST["github"];
                 }
+                if (!empty($_POST["linkedIn"])) {
+                  $user->update = $_POST["linkedIn"];
+                }
+                if (!empty($_POST["facebook"])) {
+                  $user->update = $_POST["facebook"];
+                }
+                if (!empty($_POST["twitter"])) {
+                  $user->update = $_POST["twitter"];
+                }
+                if (!empty($_POST["instagram"])) {
+                  $user->update = $_POST["intagram"];
+                }
+                if (!empty($_POST["youtube"])) {
+                  $user->update = $_POST["youtube"];
+                }
+
+                if ($user->update()) {
+                  $token = array(
+                    "iss" => $iss,
+                    "aud" => $aud,
+                    "iat" => $iat,
+                    "nbf" => $nbf,
+                    "data" => array(
+                        "fullName" => $user->fullName,
+                        "email" => $user->email,
+                        "image" => $user->image,
+                        "link" => $user->link,
+                        "summary" => $user->summary,
+                        "location" => $user->location,
+                        "ocupation" => $user->ocupation,
+                        "pro" => $user->pro,
+                        "github" => $user->github,
+                        "linkedIn" => $user->linkedIn,
+                        "facebook" => $user->facebook,
+                        "twitter" => $user->twitter,
+                        "instagram" => $user->instagram,
+                        "youtube" => $user->youtube,
+                        "projects" => $user->projects,
+                      ),
+                  );
+    
+                    $jwt = JWT::encode($token, $key);
+
+                    http_response_code(200);
+                    echo json_encode(array("message" => "Success.", "jwt" => $jwt));
+                } else {
+                    http_response_code(202);
+                    echo json_encode(array("message" => "Unable to create user."));
+                }
+            } else {
+                http_response_code(500);
+                echo json_encode(array("message" => "Unable to upload image."));
+            }
         } else {
             http_response_code(202);
             echo json_encode(array("message" => "Email does not exists."));
